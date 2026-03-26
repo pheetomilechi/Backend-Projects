ff43454da9e214d7b52f7325c2b0088e994866b6
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 09:04:04 +0000

    updated the server2.js by passing functions and changing the ports

--- Backend/server2.js
+++ Backend/server2.js
@@ -1,8 +1,20 @@
-const { application } = require("express");
+const express  = require("express");
+const app = express();
+const PORT = 3001;
+const heavyTask = () => {
+    const crypto = require ("crypto")
+}
 
-application.get('/slow',(req,res) => {
+app.get('/slow',(req,res) => {
     console.log("Slow start");
     heavyTask();
     console.log("Slow end");
     res.send('Slow response');
 });
+app.get('/fast', (req,res) => {
+    console.log("Fast route hit");
+    res.send('Fast response');
+});
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`)
+}); 
