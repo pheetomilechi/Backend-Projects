9e08d60350f411dc134b0ae56b4878591a90db1d
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 11:08:15 +0000

    added 't' to process.nextTick object

--- task-runner3.js
+++ task-runner3.js
@@ -1,8 +1,9 @@
 const { runTasks } = require("./task-runner");
 
+
 runTasks([
     () => console.log("A"),
-    () => process.nexTick(() => console.log("B(nextTick)")),
+    () => process.nextTick(() => console.log("B(nextTick)")),
     () => Promise.resolve().then(() => console.log("C(promise)")),
     () => setTimeout(() => console.log("D(timeout)"),0)
 ]);
