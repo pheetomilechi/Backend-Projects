90541c63d352b64656e45141df751a3711637dec
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 09:54:09 +0000

    Added a worker.js file to the project and tested it

--- Backend/worker.js
+++ Backend/worker.js
@@ -1,6 +1,21 @@
-const { Worker } = require('worker_threads');
+const express = require("express")
+const { Worker, parentPort } = require('worker_threads');
+const app = express();
+const PORT = 3002; 
 
-const { parentPort } = require('worker_threads');
 const crypto = require('crypto');
 crypto.pbkdf2Sync("password", "salt", 1000000, 64, "sha512");
+console.log(parentPort);
 parentPort.postMessage("Done");
+
+
+app.get('/slow', (req, res) => {
+    const worker = new Worker('./worker.js');
+    worker.on('message', () => {
+        res.send('Slow (non-blocking)');
+    });
+});
+
+app.listen (PORT, () => {
+    console.log(`worker server running on ${PORT}`)
+});
